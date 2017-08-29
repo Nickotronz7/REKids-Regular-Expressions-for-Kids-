3060e280c8c327d32787a8394ef455d791722cd3
Nickotronz7 <nickolasrc7@gmail.com>
2017-08-28 18:42:56 -0600

    Botones

---
+++ app/src/main/java/com/example/nicko/rekids_regular_expressions_for_kids_/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nicko.rekids_regular_expressions_for_kids_;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
