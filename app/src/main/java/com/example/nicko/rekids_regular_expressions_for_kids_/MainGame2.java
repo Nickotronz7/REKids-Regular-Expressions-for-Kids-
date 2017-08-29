d35d23ea3e595be11694de754f94ec77036e5096
Nickotronz7 <nickolasrc7@gmail.com>
2017-08-28 22:34:20 -0600

    Estructura básica del segundo juego y link del btn2 de la main activity para ir al segundo juego

---
+++ app/src/main/java/com/example/nicko/rekids_regular_expressions_for_kids_/MainGame2.java
@@ -0,0 +1,13 @@
+package com.example.nicko.rekids_regular_expressions_for_kids_;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainGame2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_game2);
+    }
+}
