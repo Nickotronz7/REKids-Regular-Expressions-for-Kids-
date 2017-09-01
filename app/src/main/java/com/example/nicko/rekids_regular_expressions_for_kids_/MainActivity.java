724e22ac1531d88b4db2a5bd3537f9195a9932f7
Nickotronz7 <nickolasrc7@gmail.com>
2017-09-01 00:57:20 -0600

    update de files

--- app/src/main/java/com/example/nicko/rekids_regular_expressions_for_kids_/MainActivity.java
+++ app/src/main/java/com/example/nicko/rekids_regular_expressions_for_kids_/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.nicko.rekids_regular_expressions_for_kids_;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
